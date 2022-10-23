61e5c72dd5bca96fcefca114fa477a02f06066ed
Ivan Nikitin <vantoozz@gmail.com>
2022-10-23 13:44:56 -0400

    + publish to local

--- buildSrc/src/main/kotlin/io.github.vantoozz.kli.kotlin-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.vantoozz.kli.kotlin-library-conventions.gradle.kts
@@ -2,6 +2,49 @@ plugins {
     id("io.github.vantoozz.kli.kotlin-common-conventions")
     id("org.jetbrains.kotlinx.kover")
     `java-library`
+    `maven-publish`
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("kli") {
+            from(components["java"])
+            groupId = "io.github.vantoozz.kli"
+            version = "0.0.1"
+//            afterEvaluate {
+//                artifactId = tasks.jar.get().archiveBaseName.get()
+//            }
+
+            pom {
+                name.set("KLI")
+                description.set("CLI-applications framework for Kotlin")
+                url.set("https://github.com/vantoozz/kli")
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("vantoozz")
+                        name.set("Ivan Nikitin")
+                        email.set("vantoozz@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/vantoozz/kli.git")
+                    developerConnection.set("scm:git:https://github.com/vantoozz/kli.git")
+                    url.set("https://github.com/vantoozz/kli")
+                }
+            }
+        }
+    }
 }
 
 kover {
