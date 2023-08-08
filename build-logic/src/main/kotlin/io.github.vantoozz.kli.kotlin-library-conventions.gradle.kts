0d475ec4bd1b6f38324bd90a0cfffedb126139af
Ivan Nikitin <vantoozz@gmail.com>
2023-08-08 12:00:00 -0400

    ~ version

--- build-logic/src/main/kotlin/io.github.vantoozz.kli.kotlin-library-conventions.gradle.kts
+++ build-logic/src/main/kotlin/io.github.vantoozz.kli.kotlin-library-conventions.gradle.kts
@@ -35,7 +35,7 @@ publishing {
         create<MavenPublication>("kli") {
             from(components["java"])
             groupId = "io.github.vantoozz.kli"
-            version = "0.2.0"
+            version = "0.3.0"
 
             pom {
                 url.set("https://github.com/vantoozz/kli")
