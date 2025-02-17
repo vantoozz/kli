60198307221fe231b29df5dbd6a54c3c493b6928
Ivan Nikitin <vantoozz@gmail.com>
2025-02-17 14:08:43 -0500

    ~ config loader

--- config/src/main/kotlin/io/github/vantoozz/kli/config/loader.kt
+++ config/src/main/kotlin/io/github/vantoozz/kli/config/loader.kt
@@ -1,16 +1,21 @@
 package io.github.vantoozz.kli.config
 
-import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.ExperimentalHoplite
 
+@OptIn(ExperimentalHoplite::class)
 inline fun <reified T : KliConfig> yamlConfigLoader(): (String?) -> T = { environment ->
     T::class.takeIf {
         it == KliConfig::class
     }?.let {
         object : KliConfig {} as T
-    } ?: ConfigLoader().loadConfigOrThrow(
-        listOfNotNull(
-            environment?.let { "/config.$environment.yaml" },
-            "/config.yaml"
+    } ?: ConfigLoaderBuilder.default()
+        .withExplicitSealedTypes()
+        .build()
+        .loadConfigOrThrow(
+            listOfNotNull(
+                environment?.let { "/config.$environment.yaml" },
+                "/config.yaml"
+            )
         )
-    )
 }
